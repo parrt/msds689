ccf44be6eb5242c03db135c7480ce48d14749d1a
parrt <parrt@cs.usfca.edu>
2019-01-08 14:13:27 -0800

    Initial add

--- projects/oohtable/oohtable.md
+++ projects/oohtable/oohtable.md
@@ -9,19 +9,113 @@ As you work on this project, working from your previous codebase, I encourage yo
 * Would you use different variable names now?
 * Could you improve your code, given your enhanced experience?
 
-Create `HashTable` class.
+## Discussion and requirements
+
+Using the hash table you created in MSDS692 was a bit awkward. For example, here is how we created a hash table with 5 buckets and stored 99 at the key `parrt`:
+
+```python
+table = htable(5)
+htable_put(table, "parrt", 99)
+```
+
+For this project, you are going to build a class definition called `HashTable` in file `oohtable.py` so that we can say the following instead:
+
+```python
+table = HashTable(5)
+table["parrt"] = 99
+```
+
+Similarly, to fetch something out of the table, we used to have to say:
+
+```python
+x = htable_get(table, "parrt")
+```
+
+But, now we can use the indexing operator:
+
+```python
+x = table["parrt"]
+```
+
+Your implementation of `HashTable` must define methods `__setitem__` and `__getitem__` to implement these operators (see operator overloading discussion).
+
+To count the elements within the hash table we want to be able to use `len(table)`. To do that, you have to implement the `__len__` method. Note this is counting the number of elements among all of the buckets, not the number of buckets.
+
+To iterate through the keys of your hash table like this:
+
+```python
+for k in table:
+    print(k)
+```
+
+you need to implement the `__iter__` method.  That method can return a so-called generator expression that iterates over the `keys()`.
+
+To test for membership of a key like this:
+
+```
+if 'parrt' in table:
+    print("woot!")
+```
+
+you must implement `__contains__`.
+
+To get a list of the keyvalue pairs as tuples like `[('the', 9), ('and', 9), ...]`, implement the `items` method.
+
+As with a built-in dictionary from Python, converting something to string using `str(table)` or just `print(table)` should also work. That means you have to implement the `__str__` method to print out the key:value pairs. We also had a method that dumped out all the buckets for debugging purposes called `htable_buckets_str()`.  Instead, we use `__repr__` with the class definition to print out a representation of that object.
+
+Here are all of the methods that you must implement in the class definition:
 
 <img src="images/oohtable-methods.png" width="220">
 
-# Deliverables
+Of course you can implement other methods, but those are the ones used by the test rig. (My class also has `hashcode()`, for example.)
 
-You must complete and add these to the root of your `oohtable-`*userid* repository in USF-MSDS689 organization.
+Please note that you cannot have any global variables, variable sitting outside of your class or method definitions. You can only have local variables, parameters of functions, and fields of the `HashTable` class.
+
+Your `oohtable.py` file also should not have code that gets executed when other files import `oohtable.py`.
+
+The tests described in the next section should execute very quickly, as in less than a second.
+
+## Testing 
+
+The unit tests I have provided for you test all of the functions described above. Because they all use the index notation, `table[key]`, you should get `__setitem__` and `__getitem__` working first.
+
+Please make a copy of [test_oohtable.py](test_oohtable.py) in the same directory as your `oohtable.py` file and then run your tests either from your development environment or the command line. Ultimately, you want the test results to look like the following:
+
+```
+$ python -m pytest -v test_oohtable.py 
+======================================== test session starts =========================================
+platform darwin -- Python 3.6.7, pytest-3.8.0, py-1.6.0, pluggy-0.7.1 -- /Users/parrt/anaconda3/bin/python
+cachedir: .pytest_cache
+rootdir: /Users/parrt/courses/msds689-private/projects/oohtable, inifile:
+plugins: remotedata-0.3.0, openfiles-0.3.0, doctestplus-0.1.3, arraydiff-0.2
+collected 18 items                                                                                   
+
+test_oohtable.py::test_empty PASSED                                                            [  5%]
+test_oohtable.py::test_single PASSED                                                           [ 11%]
+test_oohtable.py::test_get0 PASSED                                                             [ 16%]
+test_oohtable.py::test_get PASSED                                                              [ 22%]
+test_oohtable.py::test_singleton PASSED                                                        [ 27%]
+test_oohtable.py::test_int_to_int PASSED                                                       [ 33%]
+test_oohtable.py::test_str_to_str PASSED                                                       [ 38%]
+test_oohtable.py::test_str_to_list PASSED                                                      [ 44%]
+test_oohtable.py::test_replace_str PASSED                                                      [ 50%]
+test_oohtable.py::test_len0 PASSED                                                             [ 55%]
+test_oohtable.py::test_len PASSED                                                              [ 61%]
+test_oohtable.py::test_items0 PASSED                                                           [ 66%]
+test_oohtable.py::test_items PASSED                                                            [ 72%]
+test_oohtable.py::test_iter0 PASSED                                                            [ 77%]
+test_oohtable.py::test_iter PASSED                                                             [ 83%]
+test_oohtable.py::test_keys0 PASSED                                                            [ 88%]
+test_oohtable.py::test_keys PASSED                                                             [ 94%]
+test_oohtable.py::test_wordfreq PASSED                                                         [100%]
+
+===================================== 18 passed in 0.08 seconds ======================================
+```
+
+## Deliverables
+
+You must complete and add the following file to the root of your `oohtable-`*userid* repository in the USF-MSDS689 organization.
 
 * oohtable.py
-* test_oohtable.py (copy this from starterkit unchanged)
 
-Ultimately, you want the test results to look like the following:
-
-```
-```
 
--- projects/oohtable/test_oohtable.py
+++ projects/oohtable/test_oohtable.py
@@ -1,195 +1,143 @@
-from oohtable import *
-
-def test_empty():
-    table = HashTable(5)
-    assert str(table) == "{}"
-    assert table.__repr__() == """0000->
-0001->
-0002->
-0003->
-0004->
+"""
+A HashTable represented as a list of lists with open hashing.
+Each bucket is a list of (key,value) tuples
 """
 
-
-def test_single():
-    table = HashTable(5)
-    table["parrt"] = 99
-    assert str(table) == "{parrt:99}"
-    assert table.__repr__() == """0000->
-0001->
-0002->
-0003->parrt:99
-0004->
-"""
-
-def test_singleton():
-    table = HashTable(5)
-    table["parrt"] = {99}
-    assert str(table) == "{parrt:{99}}"
-    assert table.__repr__() == """0000->
-0001->
-0002->
-0003->parrt:{99}
-0004->
-"""
+class HashTable:
+    def __init__(self, nbuckets):
+        """Init with a list of nbuckets lists"""
+        self.buckets = [[] for i in range(nbuckets)]
 
 
-def test_int_to_int():
-    table = HashTable(5)
-    for i in range(1, 11):
-        table[i] = i
-    s = str(table)
-    assert s=="{5:5, 10:10, 1:1, 6:6, 2:2, 7:7, 3:3, 8:8, 4:4, 9:9}"
-    s = table.__repr__()
-    assert s == """0000->5:5, 10:10
-0001->1:1, 6:6
-0002->2:2, 7:7
-0003->3:3, 8:8
-0004->4:4, 9:9
-"""
+    def __len__(self):
+        n = 0
+        for i in range(len(self.buckets)):
+            n += len(self.buckets[i])
+        return n
 
 
-def test_str_to_str():
-    table = HashTable(5)
-    table["a"] = "x"
-    table["b"] = "y"
-    table["c"] = "z"
-    table["f"] = "i"
-    table["g"] = "j"
-    table["k"] = "k"
-    s = str(table)
-    assert s=='{a:x, f:i, k:k, b:y, g:j, c:z}', "found "+s
-    s = table.__repr__()
-    assert s == """0000->
-0001->
-0002->a:x, f:i, k:k
-0003->b:y, g:j
-0004->c:z
-"""
+    def __setitem__(self, key, value):
+        """
+        Perform the equivalent of table[key] = value
+        Find the appropriate bucket indicated by key and then append (key,value)
+        to that bucket if the (key,value) pair doesn't exist yet in that bucket.
+        If the bucket for key already has a (key,value) pair with that key,
+        then replace the tuple with the new (key,value).
+        Make sure that you are only adding (key,value) associations to the buckets.
+        The type(value) can be anything. Could be a set, list, number, string, anything!
+        """
+        h = self.hashcode(key) % len(self.buckets)
+        # find existing
+        el = self.bucket_indexof(key)
+        # print(f"{key} in bucket {h} element {el}")
+        assoc = (key, value)
+        if el is not None: # replace
+            self.buckets[h][el] = assoc
+        else:
+            self.buckets[h].append(assoc)
 
 
-def test_str_to_list():
-    table = HashTable(5)
-    table["parrt"] = [2, 99, 3942]
-    table["tombu"] = [6, 3, 1024, 99, 102342]
-    assert str(table) == "{tombu:[6, 3, 1024, 99, 102342], parrt:[2, 99, 3942]}"
-    assert table.__repr__() == """0000->
-0001->tombu:[6, 3, 1024, 99, 102342]
-0002->
-0003->parrt:[2, 99, 3942]
-0004->
-"""
-
-def test_replace_str():
-    table = HashTable(5)
-    table["a"] = "x"
-    table["b"] = "y"
-    table["a"] = "z"
-    table["a"] = "i"
-    table["g"] = "j"
-    table["g"] = "k"
-    s = str(table)
-    assert s == '{a:i, b:y, g:k}', "found " + s
-    s = table.__repr__()
-    assert s == """0000->
-0001->
-0002->a:i
-0003->b:y, g:k
-0004->
-"""
-
-def test_len0():
-    table = HashTable(5)
-    assert len(table)==0
+    def __getitem__(self, key):
+        """
+        Return the equivalent of table[key].
+        Find the appropriate bucket indicated by the key and look for the
+        association with the key. Return the value (not the key and not
+        the association!). Return None if key not found.
+        """
+        h = self.hashcode(key) % len(self.buckets)
+        i = self.bucket_indexof(key)
+        if i is not None:
+            (k,v) = self.buckets[h][i]
+            return v
+        return None
 
 
-def test_len():
-    table = HashTable(5)
-    table["a"] = "x"
-    table["b"] = "y"
-    table["a"] = "z"
-    table["a"] = "i"
-    table["g"] = "j"
-    table["g"] = "k"
-    assert len(table)==3
+    def __contains__(self, key):
+        return self[key] is not None
 
 
-def test_items0():
-    table = HashTable(500)
-    assert str(table.items())=="[]"
+    def __iter__(self):
+        return (key for key in self.keys())
+        pass
 
 
-def test_items():
-    table = HashTable(5)
-    table["parrt"] = {99}
-    assert str(table.items())=="[('parrt', {99})]"
+    def keys(self):
+        elems = []
+        for i in range(len(self.buckets)):
+            elems.extend( [key for key,value in self.buckets[i]] )
+        return elems
 
 
-def test_iter0():
-    table = HashTable(5)
-    keys = []
-    for k in table:
-        keys.append(k)
-    assert str(keys)=="[]"
+    def items(self):
+        elems = []
+        for i in range(len(self.buckets)):
+            elems.extend( self.buckets[i] )
+        return elems
 
 
-def test_iter():
-    table = HashTable(5)
-    table["a"] = "x"
-    table["b"] = "y"
-    table[100] = ['tom','mary']
-    table["parrt"] = {99}
-    keys = []
-    for k in table:
-        keys.append(k)
-    assert keys==[100, 'a', 'b', 'parrt']
+    def __repr__(self):
+        """
+        Return a string representing the various buckets of this table.
+        The output looks like:
+            0000->
+            0001->
+            0002->
+            0003->parrt:99
+            0004->
+        where parrt:99 indicates an association of (parrt,99) in bucket 3.
+        """
+        s = ""
+        for i in range(len(self.buckets)):
+            bucketlist = self.buckets[i]
+            s += "%04d" % i + "->"
+            bucket = [str(k)+":"+str(v) for (k,v) in bucketlist]
+            s += ", ".join(bucket) + "\n"
+        return s
 
 
-def test_keys0():
-    table = HashTable(5)
-    assert table.keys()==[]
+    def __str__(self):
+        """
+        Return what str(table) would return for a regular Python dict
+        such as {parrt:99}. The order should be in bucket order and then
+        insertion order within each bucket. The insertion order is
+        guaranteed when you append to the buckets in htable_put().
+        """
+        assocs = []
+        for i in range(len(self.buckets)):
+            bucketlist = self.buckets[i]
+            bucket = [str(k)+":"+str(v) for (k,v) in bucketlist]
+            assocs.extend(bucket)
+        return "{" + ", ".join(assocs) + "}"
 
 
-def test_keys():
-    table = HashTable(5)
-    table["a"] = "x"
-    table["b"] = "y"
-    table[100] = ['tom','mary']
-    table["parrt"] = {99}
-    assert table.keys()==[100, 'a', 'b', 'parrt']
+    def hashcode(self, o):
+        """
+        Return a hashcode for strings and integers; all others return None
+        For integers, just return the integer value.
+        For strings, perform operation h = h*31 + ord(c) for all characters in the string
+        """
+        if isinstance(o, str):
+            h = 0
+            for c in o:
+                h = h*31 + ord(c)
+            return h
+        elif isinstance(o, int):
+            return o
+        return None
 
+    def bucket_indexof(self, key):
+        """
+        You don't have to implement this, but I found it to be a handy function.
 
-def test_wordfreq():
-    text = """
-    The Road Not Taken by Robert Frost
-    
-    Two roads diverged in a yellow wood,
-    And sorry I could not travel both
-    And be one traveler, long I stood
-    And looked down one as far as I could
-    To where it bent in the undergrowth;
-    Then took the other, as just as fair,
-    And having perhaps the better claim,
-    Because it was grassy and wanted wear;
-    Though as for that the passing there
-    Had worn them really about the same,
-    And both that morning equally lay
-    In leaves no step had trodden black.
-    Oh, I kept the first for another day!
-    Yet knowing how way leads on to way,
-    I doubted if I should ever come back.
-    I shall be telling this with a sigh
-    Somewhere ages and ages hence:
-    Two roads diverged in a wood, and I-
-    I took the one less traveled by,
-    And that has made all the difference.
-    """
-    words = text.lower().split()
-    h = HashTable(101)
-    for w in words:
-        if w not in h:
-            h[w] = 0
-        h[w] = h[w] + 1
-    s = str(sorted(h.items(), key=lambda kv: kv[1], reverse=True)) # convert to counter to normalize; needs __iter__() to work
-    assert s == """[('the', 9), ('and', 9), ('i', 8), ('as', 5), ('in', 4), ('that', 3), ('one', 3), ('a', 3), ('be', 2), ('diverged', 2), ('had', 2), ('ages', 2), ('two', 2), ('it', 2), ('roads', 2), ('could', 2), ('took', 2), ('to', 2), ('for', 2), ('wood,', 2), ('not', 2), ('both', 2), ('black.', 1), ('hence:', 1), ('bent', 1), ('them', 1), ('by,', 1), ('then', 1), ('passing', 1), ('back.', 1), ('perhaps', 1), ('travel', 1), ('sigh', 1), ('on', 1), ('less', 1), ('all', 1), ('was', 1), ('leads', 1), ('wanted', 1), ('morning', 1), ('robert', 1), ('about', 1), ('way', 1), ('if', 1), ('undergrowth;', 1), ('knowing', 1), ('by', 1), ('step', 1), ('kept', 1), ('sorry', 1), ('other,', 1), ('this', 1), ('made', 1), ('really', 1), ('telling', 1), ('yellow', 1), ('road', 1), ('far', 1), ('fair,', 1), ('wear;', 1), ('stood', 1), ('better', 1), ('doubted', 1), ('day!', 1), ('yet', 1), ('there', 1), ('has', 1), ('should', 1), ('somewhere', 1), ('just', 1), ('oh,', 1), ('worn', 1), ('same,', 1), ('difference.', 1), ('lay', 1), ('claim,', 1), ('leaves', 1), ('another', 1), ('long', 1), ('because', 1), ('i-', 1), ('come', 1), ('traveled', 1), ('equally', 1), ('first', 1), ('where', 1), ('grassy', 1), ('how', 1), ('down', 1), ('way,', 1), ('frost', 1), ('shall', 1), ('no', 1), ('ever', 1), ('having', 1), ('traveler,', 1), ('though', 1), ('taken', 1), ('trodden', 1), ('with', 1), ('looked', 1)]"""
-
+        Return the index of the element within a specific bucket; the bucket is:
+        table[hashcode(key) % len(table)]. You have to linearly
+        search the bucket to find the tuple containing key.
+        """
+        h = self.hashcode(key) % len(self.buckets)
+        bucket = self.buckets[h]
+        el = None
+        for i in range(len(bucket)):
+            (k, v) = bucket[i]
+            if k == key:
+                el = i
+        return el
