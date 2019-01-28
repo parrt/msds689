18ea0ed745902a7d3a61b288576f35624abe3201
parrt <parrt@cs.usfca.edu>
2019-01-28 13:13:50 -0800

    update test

--- projects/oohtable/test_oohtable.py
+++ projects/oohtable/test_oohtable.py
@@ -1,4 +1,5 @@
 from oohtable import *
+import pandas as pd
 
 def test_empty():
     table = HashTable(5)
@@ -197,10 +198,115 @@ def test_wordfreq():
     And that has made all the difference.
     """
     words = text.lower().split()
+    # Use student HashTable
     h = HashTable(101)
     for w in words:
         if w not in h:
             h[w] = 0
         h[w] = h[w] + 1
-    s = str(sorted(h.items(), key=lambda kv: kv[1], reverse=True)) # convert to counter to normalize; needs __iter__() to work
-    assert s == """[('the', 9), ('and', 9), ('i', 8), ('as', 5), ('in', 4), ('that', 3), ('one', 3), ('a', 3), ('be', 2), ('diverged', 2), ('had', 2), ('ages', 2), ('two', 2), ('it', 2), ('roads', 2), ('could', 2), ('took', 2), ('to', 2), ('for', 2), ('wood,', 2), ('not', 2), ('both', 2), ('black.', 1), ('hence:', 1), ('bent', 1), ('them', 1), ('by,', 1), ('then', 1), ('passing', 1), ('back.', 1), ('perhaps', 1), ('travel', 1), ('sigh', 1), ('on', 1), ('less', 1), ('all', 1), ('was', 1), ('leads', 1), ('wanted', 1), ('morning', 1), ('robert', 1), ('about', 1), ('way', 1), ('if', 1), ('undergrowth;', 1), ('knowing', 1), ('by', 1), ('step', 1), ('kept', 1), ('sorry', 1), ('other,', 1), ('this', 1), ('made', 1), ('really', 1), ('telling', 1), ('yellow', 1), ('road', 1), ('far', 1), ('fair,', 1), ('wear;', 1), ('stood', 1), ('better', 1), ('doubted', 1), ('day!', 1), ('yet', 1), ('there', 1), ('has', 1), ('should', 1), ('somewhere', 1), ('just', 1), ('oh,', 1), ('worn', 1), ('same,', 1), ('difference.', 1), ('lay', 1), ('claim,', 1), ('leaves', 1), ('another', 1), ('long', 1), ('because', 1), ('i-', 1), ('come', 1), ('traveled', 1), ('equally', 1), ('first', 1), ('where', 1), ('grassy', 1), ('how', 1), ('down', 1), ('way,', 1), ('frost', 1), ('shall', 1), ('no', 1), ('ever', 1), ('having', 1), ('traveler,', 1), ('though', 1), ('taken', 1), ('trodden', 1), ('with', 1), ('looked', 1)]"""
+
+    # Test using dataframe
+    df = pd.DataFrame(data=list(h.items()), columns=['word','count'])
+    df = df.sort_values(['count','word'], ascending=[False,True])
+    assert str(df.values)==\
+"""[['and' 9]
+ ['the' 9]
+ ['i' 8]
+ ['as' 5]
+ ['in' 4]
+ ['a' 3]
+ ['one' 3]
+ ['that' 3]
+ ['ages' 2]
+ ['be' 2]
+ ['both' 2]
+ ['could' 2]
+ ['diverged' 2]
+ ['for' 2]
+ ['had' 2]
+ ['it' 2]
+ ['not' 2]
+ ['roads' 2]
+ ['to' 2]
+ ['took' 2]
+ ['two' 2]
+ ['wood,' 2]
+ ['about' 1]
+ ['all' 1]
+ ['another' 1]
+ ['back.' 1]
+ ['because' 1]
+ ['bent' 1]
+ ['better' 1]
+ ['black.' 1]
+ ['by' 1]
+ ['by,' 1]
+ ['claim,' 1]
+ ['come' 1]
+ ['day!' 1]
+ ['difference.' 1]
+ ['doubted' 1]
+ ['down' 1]
+ ['equally' 1]
+ ['ever' 1]
+ ['fair,' 1]
+ ['far' 1]
+ ['first' 1]
+ ['frost' 1]
+ ['grassy' 1]
+ ['has' 1]
+ ['having' 1]
+ ['hence:' 1]
+ ['how' 1]
+ ['i-' 1]
+ ['if' 1]
+ ['just' 1]
+ ['kept' 1]
+ ['knowing' 1]
+ ['lay' 1]
+ ['leads' 1]
+ ['leaves' 1]
+ ['less' 1]
+ ['long' 1]
+ ['looked' 1]
+ ['made' 1]
+ ['morning' 1]
+ ['no' 1]
+ ['oh,' 1]
+ ['on' 1]
+ ['other,' 1]
+ ['passing' 1]
+ ['perhaps' 1]
+ ['really' 1]
+ ['road' 1]
+ ['robert' 1]
+ ['same,' 1]
+ ['shall' 1]
+ ['should' 1]
+ ['sigh' 1]
+ ['somewhere' 1]
+ ['sorry' 1]
+ ['step' 1]
+ ['stood' 1]
+ ['taken' 1]
+ ['telling' 1]
+ ['them' 1]
+ ['then' 1]
+ ['there' 1]
+ ['this' 1]
+ ['though' 1]
+ ['travel' 1]
+ ['traveled' 1]
+ ['traveler,' 1]
+ ['trodden' 1]
+ ['undergrowth;' 1]
+ ['wanted' 1]
+ ['was' 1]
+ ['way' 1]
+ ['way,' 1]
+ ['wear;' 1]
+ ['where' 1]
+ ['with' 1]
+ ['worn' 1]
+ ['yellow' 1]
+ ['yet' 1]]"""
