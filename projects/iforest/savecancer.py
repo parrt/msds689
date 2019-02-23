3a61d0b2205f24b2845e57fbcec3cdc937760d06
parrt <parrt@cs.usfca.edu>
2019-02-23 11:48:03 -0800

    tweak

--- projects/iforest/savecancer.py
+++ projects/iforest/savecancer.py
@@ -4,4 +4,7 @@ import pandas as pd
 cancer = load_breast_cancer()
 df = pd.DataFrame(data=cancer.data, columns=cancer.feature_names)
 df['diagnosis'] = cancer.target
+df.loc[df.diagnosis==0,'diagnosis'] = -1
+df.loc[df.diagnosis==1,'diagnosis'] = 0
+df.loc[df.diagnosis==-1,'diagnosis'] = 1
 df.to_csv("cancer.csv", index=False)
